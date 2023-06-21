94059b157680dd1044db272e58006598791bddec
brokensilver75 <rsmadhan75@gmail.com>
2023-06-21 10:21:32 +0530

    Changed Logic for ball velocity

--- BrickBreak_Thingy/Assets/Scripts/MoveBall.cs
+++ BrickBreak_Thingy/Assets/Scripts/MoveBall.cs
@@ -5,44 +5,45 @@ using UnityEngine;
 public class MoveBall : MonoBehaviour
 {
     [SerializeField] GameObject P1, P2, Ball;
-    [SerializeField] float force, offset;
+    [SerializeField] float force, offset, ballVelocity = 5f;
     Rigidbody ballRigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         ballRigidBody = Ball.GetComponent<Rigidbody>();
-        ballRigidBody.AddForce(0, -force, 0);
+        ballRigidBody.velocity = new Vector3(0, -ballVelocity, 0) * Time.deltaTime;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MaintainBallVelocity();
     }
+
+    private void MaintainBallVelocity()
+    {
+        if (ballRigidBody.velocity.y < 0)
+            ballRigidBody.velocity = new Vector3(0, -ballVelocity, 0) * Time.deltaTime;
+        else
+            ballRigidBody.velocity = new Vector3(0, ballVelocity, 0) * Time.deltaTime;
+    }
+
     void OnCollisionEnter(Collision other)
     {
         
         if (other.gameObject.tag == "P1")
         {
             Ball.transform.position = P2.transform.position + new Vector3(0, -offset, 0);
-            /*Destroy(Ball);
-            Instantiate(Ball, P2.transform.position + new Vector3(0, -offset, 0) , Quaternion.identity);
-            if (ballRigidBody.velocity.y == 0f)
-            {
-                ballRigidBody.AddForce(0, force, 0);
-            }*/
-            
         }
         if (other.gameObject.tag == "P2")
         {
             Ball.transform.position = P1.transform.position + new Vector3(0, offset, 0);
-            /*Destroy(Ball);
-            Instantiate(Ball, P1.transform.position + new Vector3(0, offset, 0), Quaternion.identity);
-            if (ballRigidBody.velocity.y == 0f)
-            {
-                ballRigidBody.AddForce(0, -force, 0);
-            }*/
+        }
+        if (other.gameObject.tag == "Obstacle")
+        {
+            ballRigidBody.velocity *= -1f;
         }
     }
 }
