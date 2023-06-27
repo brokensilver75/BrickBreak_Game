36c3cd87f13383db98bff0a78c33d22196665156
brokensilver75 <rsmadhan75@gmail.com>
2023-06-27 10:29:34 +0530

    ufo no longer spawns from hell

--- BrickBreak_Thingy/Assets/Scripts/SpawnUFO.cs
+++ BrickBreak_Thingy/Assets/Scripts/SpawnUFO.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class SpawnUFO : MonoBehaviour
 {
-    [SerializeField] GameObject ufo;
-    bool ufoInField;
+    [SerializeField] GameObject ufoPrefab;
+    GameObject newUFO;
+    [SerializeField] float spawnInterval = 3f;
+    Vector3 offset = new Vector3(-3f, 0, 0);
+    Quaternion ufoRotation = new Quaternion(45f, 0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        ufoInField = true;
+        StartCoroutine(spawnUFO(spawnInterval, ufoPrefab));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(GameObject.Find("UFO") == null)
-        {
-            ufoInField = false;
-        }
+        
+    }
 
-        if (!ufoInField)
-        {
-            Instantiate(ufo, transform.position, Quaternion.identity);
-            ufoInField = true;
-        }
+    private IEnumerator spawnUFO(float interval, GameObject ufo)
+    {
+        yield return new WaitForSeconds(interval);
+        newUFO = Instantiate(ufo, transform.position + offset, ufoPrefab.transform.rotation);
+        StartCoroutine(spawnUFO(interval, ufo));
     }
 }
 
