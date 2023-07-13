46a20b71b02a05a103fede62671fd2d56c3b269a
brokensilver75 <rsmadhan75@gmail.com>
2023-07-13 11:58:31 +0530

    shield color changes

--- BrickBreak Thingy URP/Assets/Scripts/ShieldCollisions.cs
+++ BrickBreak Thingy URP/Assets/Scripts/ShieldCollisions.cs
@@ -6,20 +6,36 @@ public class ShieldCollisions : MonoBehaviour
 {
     // Start is called before the first frame update
     float health = 100f;
+    [Header("Intensity of shield")]
+    [SerializeField] float intensity = 3.2f;
     Material shieldMaterial;
     Shader shieldShader; 
+    Color[] shieldColor = {new Color(0.1098f, 1.4980f, 1.4274f, 1f),
+                           new Color(1.3200f, 1.4980f, 0.1098f, 1f),
+                           new Color(1.4980f, 0.6240f, 0.1098f, 1f),
+                           new Color(1.4980f, 0.1098f, 0.1362f, 1f)};
+
     void Start()
     {
         shieldMaterial = GetComponent<MeshRenderer>().sharedMaterial;
-        shieldShader = GetComponent<Shader>();
-
-        Debug.Log (shieldMaterial.GetColor("_Base"));
-        
+        shieldShader = GetComponent<Shader>();        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (health <= 100 && health >= 75)
+            shieldMaterial.SetColor("_Base", shieldColor[0] * intensity);
+            
+        if (health < 75 && health >= 50)
+            shieldMaterial.SetColor("_Base", shieldColor[1] * intensity);
+            
+        if (health < 50 && health >= 25)
+            shieldMaterial.SetColor("_Base", shieldColor[2] * intensity);
+            
+        if (health < 25)
+            shieldMaterial.SetColor("_Base", shieldColor[3] * intensity);
+            
         if (health <= 0)
         {
             Destroy (gameObject);
@@ -32,8 +48,6 @@ public class ShieldCollisions : MonoBehaviour
         {
             Destroy(other.gameObject);  
             health -= 10f;
-            shieldMaterial.SetColor("_Base", Color.red);
-                    
         }
     }
 }
